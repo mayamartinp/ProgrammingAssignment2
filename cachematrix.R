4ce918214a2c20d3613deb5e6821d07b00a45685
mayamartinp <maya_martinp@hotmail.com>
2019-09-11 16:56:07 -0500

    Tarea semana 3 Lenguaje R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matri_inv <- NULL
+  
+  set_Matriz <- function(t1) {
+    x <<- t1
+    matri_inv <<- NULL
+  }
+  get_matriz <- function() x
+  set_inv <- function(inv) matri_inv <<- inv
+  get_inv <- function() matri_inv
+  list(set = set_Matriz, get = get_matriz,
+       setinv = set_inv,
+       getinv = get_inv)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  matri_1 <- x$getinv()
+  if(!is.null(matri_1)) {
+    message("getting cached data")
+    return(matri_1)
+  }
+  data <- x$get()
+  matri_1 <- solve(data)
+  x$setinv(matri_1)
+  matri_1
 }
